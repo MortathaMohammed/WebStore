1241a5c3de48bb6705f0a3c8db712178b84c184c
MortathaMohammed <MortathaMohammedAlyasiry@gmail.com>
2024-06-04 08:42:37 +0300

    implement search methode in home controller with the show products in cards in sparated page can't implement search for categories its make the search to complicated

--- Fantasia.DataAccess/obj/Debug/net8.0/Fantasia.DataAccess.AssemblyInfo.cs
+++ Fantasia.DataAccess/obj/Debug/net8.0/Fantasia.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fantasia.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b78c93ee2c3579e29e469e13a9fae936386239cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a758f43ca620e9ac48d2aae4dcbb1090a58a8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fantasia.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fantasia.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Fantasia.Mvc/Controllers/HomeController.cs
+++ Fantasia.Mvc/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Fantasia.Mvc.Models;
+using Fantasia.DataAccess.Service.IService;
 
 namespace Fantasia.Mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IUnitOfWork _unitOfWork;
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+
+    public async Task<IActionResult> GetProductsByName(string search)
+    {
+        var products = await _unitOfWork.ProductService.GetProducts();
+
+        if (!string.IsNullOrEmpty(search))
+        {
+            products = products.Where(p => p.Name!.Contains(search)).ToList();
+        }
+        return View(products);
     }
 
     public IActionResult Privacy()
